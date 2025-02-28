333c30ba3a96f6f1ee70bd1c65f737d4eb440c95
Homer <12595601+ihomerr@users.noreply.github.com>
2025-02-28 09:34:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,8 +29,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '最新动态', url: '/tag/最新动态' },
-  HEO_HERO_CATEGORY_3: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '最新动态', url: '/tag/最新动态' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
